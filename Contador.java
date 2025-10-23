da46c9ff66aca901a66dc734ff69c62c2d2d84c1
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2025-10-23 10:18:20 +0200

    Update Contador.java and Add factorial

--- Contador.java
+++ Contador.java
@@ -1,8 +1,24 @@
 public class Contador {
     public static void contar(int n){
-        System.out.println();
+        System.out.println(n);
+        if (n == 0){
+            System.out.println("---");
+            return;
+        }
+        n--;
+        contar(n);
+    }
+    public static void factorial(int n){
+        if (n == 1){
+            System.out.println(1);
+            return;
+        }
+        System.out.println(n * (n-1));
+        n--;
+        factorial(n);
     }
     public static void main(String[] args) {
-        contar(6);
+        contar(10);
+        factorial(5);
     }
 }
